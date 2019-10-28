a29159bd8967825c72f59fa506eabd27fe6e4579
krkr1303 <krkr@dsb.dk>
2019-10-28 13:41:52 +0100

    Change default port

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,10 +3,15 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+
+        application.run(args);
     }
 }
